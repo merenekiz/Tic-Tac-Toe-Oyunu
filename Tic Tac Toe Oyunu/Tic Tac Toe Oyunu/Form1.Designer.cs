96bae2b835f97640b6cba4824e53081800163e3f
merenekiz <146181787+merenekiz@users.noreply.github.com>
2024-11-08 15:14:05 +0300

    Add files via upload

---
+++ Tic Tac Toe Oyunu/Tic Tac Toe Oyunu/Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace Tic_Tac_Toe_Oyunu
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.zamanlayıcı = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Corbel", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.Color.Fuchsia;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(138, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Oyuncu Skoru:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Corbel", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.ForeColor = System.Drawing.Color.Blue;
+            this.label2.Location = new System.Drawing.Point(338, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(154, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Bilgisayar Skoru:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(91, 139);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 100);
+            this.button1.TabIndex = 2;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.oyuncuhareketi);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(197, 139);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 100);
+            this.button2.TabIndex = 3;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.oyuncuhareketi);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(303, 139);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(100, 100);
+            this.button3.TabIndex = 4;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.oyuncuhareketi);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(91, 245);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(100, 100);
+            this.button4.TabIndex = 5;
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.oyuncuhareketi);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(197, 245);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(100, 100);
+            this.button5.TabIndex = 6;
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.oyuncuhareketi);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(303, 245);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(100, 100);
+            this.button6.TabIndex = 7;
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.oyuncuhareketi);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(90, 351);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(100, 100);
+            this.button7.TabIndex = 8;
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.oyuncuhareketi);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(197, 351);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(100, 100);
+            this.button8.TabIndex = 9;
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.oyuncuhareketi);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(303, 351);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(100, 100);
+            this.button9.TabIndex = 10;
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.oyuncuhareketi);
+            // 
+            // button10
+            // 
+            this.button10.BackColor = System.Drawing.Color.Gold;
+            this.button10.Font = new System.Drawing.Font("Corbel", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button10.Location = new System.Drawing.Point(144, 472);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(200, 50);
+            this.button10.TabIndex = 11;
+            this.button10.Text = "Oyunu Yeniden Başlat";
+            this.button10.UseVisualStyleBackColor = false;
+            this.button10.Click += new System.EventHandler(this.yenidenbaslat);
+            // 
+            // zamanlayıcı
+            // 
+            this.zamanlayıcı.Tick += new System.EventHandler(this.pchareketi);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(526, 564);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Tic Tac Toe";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Timer zamanlayıcı;
+    }
+}
+
---
+++ Tic Tac Toe Oyunu/Tic Tac Toe Oyunu/Form1.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tic_Tac_Toe_Oyunu
+{
+    public partial class Form1 : Form
+    {
+        // Oyuncular X ve O olarak tanımlanır
+        public enum oyuncu
+        {
+            X, O
+        }
+
+        oyuncu gunceloyuncu; // Şuanki oyuncu 
+        Random random = new Random(); // Bilgisayarın rastgele hareketi için random komudu
+        int oyuncuskoru = 0; // Oyuncu Skoru
+        int pcskoru = 0; // Bilgisayar Skoru
+        List<Button> butonlar; // Ekranda butonları tutan liste
+
+
+
+
+        public Form1()
+        {
+            InitializeComponent(); // Formu başlatır
+            yenidenbaslat(); // Oyunun başlatır
+        }
+
+        // Bilgisayarın Hareketleri
+        private void pchareketi(object sender, EventArgs e)
+        {
+            // Bilgisayarın yapacağı en iyi hamleyi bulmak için kontrol 
+            Button hamle = KazanmaHamlesiBul("O") ?? KazanmaHamlesiBul("X") ?? OrtaHamleyiBul() ?? RastgeleHamle();
+
+            // Hamle bulununca
+            if (hamle != null)
+            {
+                hamle.Enabled = false;
+                gunceloyuncu = oyuncu.O;
+                hamle.Text = gunceloyuncu.ToString();
+                hamle.BackColor = Color.Red;
+                butonlar.Remove(hamle);
+                oyunkontrol();
+                zamanlayıcı.Stop();
+            }
+        }
+
+        // Kazanma veya engelleme hamlesini bulmak için
+        private Button KazanmaHamlesiBul(string oyuncu)
+        {
+            // Olası kazanç dizilerini kontrol 
+            List<Button[]> kazananDiziler = new List<Button[]>
+    {
+        new Button[] { button1, button2, button3 },
+        new Button[] { button4, button5, button6 },
+        new Button[] { button7, button8, button9 },
+        new Button[] { button1, button4, button7 },
+        new Button[] { button2, button5, button8 },
+        new Button[] { button3, button6, button9 },
+        new Button[] { button1, button5, button9 },
+        new Button[] { button3, button5, button7 }
+    };
+
+            // Her kazanan diziyi kontrol eder, iki hücrede aynı oyuncu varsa ve üçüncü hücre boşsa oraya hamle yapılır
+            foreach (var dizi in kazananDiziler)
+            {
+                if (dizi.Count(b => b.Text == oyuncu) == 2 && dizi.Any(b => b.Text == ""))
+                {
+                    return dizi.First(b => b.Text == "");
+                }
+            }
+            return null;
+        }
+
+        // Orta kareyi tercih eder
+        private Button OrtaHamleyiBul()
+        {
+            if (button5.Text == "")
+            {
+                return button5;
+            }
+            return null;
+        }
+
+        // Eğer stratejik hamle yoksa rastgele bir hamle yapar
+        private Button RastgeleHamle()
+        {
+            if (butonlar.Count > 0)
+            {
+                int index = random.Next(butonlar.Count);
+                return butonlar[index];
+            }
+            return null;
+        }
+
+        // Oyuncu hareketi
+        private void oyuncuhareketi(object sender, EventArgs e)
+        {
+            var button = (Button)sender; // Tıklanan butonu al
+            gunceloyuncu = oyuncu.X; // Oyuncunun hamlesi X olacak
+            button.Text = gunceloyuncu.ToString(); // Butona X yaz
+            button.Enabled = false; // Butonu devre dışı bırakır
+            button.BackColor = Color.Green; // Buton rengini yeşil yapar
+            butonlar.Remove(button); // Butonu listeden çıkarır
+            oyunkontrol(); // Oyun kontrolü yapılır
+            zamanlayıcı.Start(); // Zamanlayıcıyı başlatıt
+
+
+
+
+        }
+
+        // Yeniden başlat butonuna tıklanması
+        private void yenidenbaslat(object sender, EventArgs e)
+        {
+            yenidenbaslat();
+        }
+
+
+        private void oyunkontrol()
+
+            // Kazanma Kontrolü
+        {
+            if (button1.Text == "X" && button2.Text == "X" && button3.Text == "X"
+               || button4.Text == "X" && button4.Text == "X" && button6.Text == "X"
+               || button7.Text == "X" && button8.Text == "X" && button9.Text == "X"
+               || button1.Text == "X" && button4.Text == "X" && button7.Text == "X"
+               || button2.Text == "X" && button5.Text == "X" && button8.Text == "X"
+               || button3.Text == "X" && button6.Text == "X" && button9.Text == "X"
+               || button1.Text == "X" && button5.Text == "X" && button9.Text == "X"
+               || button3.Text == "X" && button5.Text == "X" && button7.Text == "X")
+            {
+                zamanlayıcı.Stop(); // Zamanlayıcıyı durdur
+                MessageBox.Show("Oyuncu Kazandı"); // Oyuncunun kazandığını bildirir
+                oyuncuskoru++; // Oyuncunun skorunu artırır
+                label1.Text = "Oyuncu Skoru: " + oyuncuskoru; // Skoru güncelle
+                yenidenbaslat(); // Oyunu yeniden başlatır
+
+            }
+            else if (button1.Text == "O" && button2.Text == "O" && button3.Text == "O"
+               || button4.Text == "O" && button4.Text == "O" && button6.Text == "O"
+               || button7.Text == "O" && button8.Text == "O" && button9.Text == "O"
+               || button1.Text == "O" && button4.Text == "O" && button7.Text == "O"
+               || button2.Text == "O" && button5.Text == "O" && button8.Text == "O"
+               || button3.Text == "O" && button6.Text == "O" && button9.Text == "O"
+               || button1.Text == "O" && button5.Text == "O" && button9.Text == "O"
+               || button3.Text == "O" && button5.Text == "O" && button7.Text == "O")
+            {
+                zamanlayıcı.Stop(); // Zamanlayıcıyı durdurur
+                MessageBox.Show("Bilgisayar Kazandı"); // Bilgisayarın kazandığını bildirir
+                pcskoru++; // Bilgisayarın skorunu artırır
+                label2.Text = "Bilgisayar Skoru: " + pcskoru; // Skoru güncelle
+                yenidenbaslat(); // Oyunu yeniden başlatır
+            }
+
+            // Beraberlik durumu
+            if (butonlar.All(b => b.Text != " "))
+            {
+                zamanlayıcı.Stop();
+                MessageBox.Show("Beraberlik!");
+                yenidenbaslat();
+            }
+        }
+
+        
+        // Oyunu yeniden başlatır
+        private void yenidenbaslat()
+        {
+            butonlar = new List<Button> { button1, button2, button3, button4, button5, button6, button7, button8, button9 };
+
+            // Tüm butonları sıfırlar 
+            foreach (Button x in butonlar)
+            {
+                x.Enabled = true;
+                x.Text = " ";
+                x.BackColor = DefaultBackColor;
+            }
+
+        }
+    }
+}
